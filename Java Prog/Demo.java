795221b7889addbf7b9f7b9a2ce548c7c6123875
yukeshchandranc-dot <yukeshchandranc@gmail.com>
2026-06-15 10:27:20 +0530

    Delete Java Prog directory

--- Java Prog/Demo.java
+++
@@ -1,2 +0,0 @@
-yukeshchandranc-dot.github.io/411624104059/folder/Demo.html
-yukeshchandranc-dot.github.io/411624104059/Demo.html
--- Java Prog/ex.java
+++
@@ -1,8 +0,0 @@
-public class ex {
-    public static void main(String[] args) {
-        String s = "Apple";
-        for (int i = 0; i < s.length(); i++) {
-            System.out.println(s.charAt(i));
-        }
-    }
-}
