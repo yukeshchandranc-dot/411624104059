fdcbfbdfa1b18eab453147d5d8551f26e49dba1f
unknown <yukeshchandranc@google.com>
2026-06-16 11:14:53 +0530

    js

---
+++ Event.js
@@ -0,0 +1,28 @@
+
+// console.log("A")
+// console.log("B")
+// console.log("C")
+// const promise=new Promise((resolve,reject)=>
+// {
+//     const s=true
+//     if(s)
+//         console.log("Done")
+//     else
+//         console.log("No")
+// })
+// promise.then(res=>{console.log(res)}).catch(err=>{console.log(err)})
+    console.log(1)
+    console.log(2)
+    console.log("Hi")
+    
+    function example(){
+        console.log("Hello")
+    }
+    const example1=()=>{
+        const a=1
+        const b=2
+        console.log(1+2)
+    }
+    setTimeout(example(),3000)
+    setTimeout(example1(),450)
+    
--- Java Prog/sumOfNnum.java
+++
@@ -1,8 +0,0 @@
-public class sumOfNnum {
-    public static void main(String[] args) {
-        int n = 10, s = 0;
-        for (int i = 1; i <= n; i++) {
-            s += i; 
-        }System.out.printf("%d", s);
-    }
-}
